92eaccb3d41b2e283d7eba92ca9d926f065dd66c
RafliNST <rafli.ran3103@gmail.com>
2026-02-05 08:54:25 +0700

    controllers

---
+++ app/controllers/Home.php
@@ -0,0 +1,12 @@
+<?php
+
+class Home {
+    public function index()
+    {
+        echo "This is the Home controller index method.";
+    }
+
+    public function test() {
+        echo "hoho";
+    }
+}
